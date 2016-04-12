d57edc63179b525f64b2b1ccc09aba61a8f50c0a
Alexandre <alexandre.chevallier4@gmail.com>
2016-04-12 15:53:19 +0200

    marche

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,12 @@
 /// <reference path="../node_modules/angular2/typings/browser.d.ts" />
-import {bootstrap}    from 'angular2/platform/browser';
+import {bootstrap} from 'angular2/platform/browser';
 import 'rxjs/Rx';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import 'rxjs/add/operator/map';
-import {AppComponent} from './app.component'
- 
+import {AppComponent} from './app.component';
+
+import {DemoService} from './demo.service';
+
 bootstrap(AppComponent, [
   HTTP_PROVIDERS,
   DemoService
