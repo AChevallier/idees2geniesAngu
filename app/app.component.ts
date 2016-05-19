48d987a6f2e859dcb597f3fd18bd2209e5830876
Alexandre <alexandre.chevallier4@gmail.com>
2016-05-19 23:35:24 +0200

    problem comp

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,75 +1,74 @@
-import {Component} from 'angular2/core';
-
-import {Injectable} from 'angular2/core';
+import {Component, OnDestroy, OnInit, Injector, DynamicComponentLoader} from 'angular2/core';
 import {Http, Response, Headers} from 'angular2/http';
-import {Observable} from 'rxjs/Rx';
 import {AppSettings} from './app.settings';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'template/my-app.html'
 })
-export class AppComponent {
- 
-  public foods;
-  public time;
-  username: string;
-  password: string;
- 
-  public receiveName;
-  constructor(private http:Http) { 
-    
-  }
+export class AppComponent{
 
-  ngOnInit() { 
-    this.getFoods();
-    this.getTime(); 
-  }
-  logError(err) {
-    console.error('There was an error: ' + err);
-  }
+    public time;
+    username:string;
+    password:string;
 
-  getFoods() {
-    this.http.get('/app/food.json').map((res: Response) => res.json()).subscribe(
-      // the first argument is a function which runs on success
-      data => { this.foods = data},
-      // the second argument is a function which runs on error
-      err => console.error(err),
-      // the third argument is a function which runs on completion
-      () => console.log('done loading foods')
-    );
-  }
+    public receiveName;
 
-  getTime() {
-    this.http.get(AppSettings.API_ENDPOINT + 'ping').map(res => res.json()).subscribe(
-      // the first argument is a function which runs on success
-    data => { this.time = JSON.parse(data) },
-      // the second argument is a function which runs on error
-      err => console.error(err),
-      // the third argument is a function which runs on completion
-      () => console.log('done loading time' + JSON.stringify(this.time))
-    );
-  }
+    public dcl;
+    public injector;
 
-  authenticate(name) {
+    constructor(dcl:DynamicComponentLoader, injector:Injector,private http:Http) {
+        this.dcl = dcl;
+        this.injector = injector;
+    }
 
-    let creds = JSON.stringify({ name: name.value });
+    ngOnInit() {
+        this.getTime();
+    }
 
-    let headers = new Headers();
-    //headers.append('Content-Type', 'application/json');
+    logError(err) {
+        console.error('There was an error: ' + err);
+    }
 
-    headers.append('Content-Type' , 'application/x-www-form-urlencoded; charset=UTF-8');
+    getTime() {
+        this.http.get(AppSettings.API_ENDPOINT + 'ping').map(res => res.json()).subscribe(
+            // the first argument is a function which runs on success
+            data => {
+                this.time = JSON.parse(data)
+            },
+            // the second argument is a function which runs on error
+            err => console.error(err),
+            // the third argument is a function which runs on completion
+            () => console.log('done loading time' + JSON.stringify(this.time))
+        );
+    }
 
-    this.http.post(AppSettings.API_ENDPOINT + 'ping/post', creds, {
-      headers: headers
-      })
-        .map(res => res.json())
-      .subscribe(
-        data => {
-          this.receiveName = JSON.parse(data);
-        },
-        err => this.logError(err.json().message),
-        () => console.log('Authentication Complete')
-      );
-  }
+    postName(name) {
+
+        let creds = JSON.stringify({name: name.value});
+
+        let headers = new Headers();
+        //headers.append('Content-Type', 'application/json');
+
+        headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+        this.http.post(AppSettings.API_ENDPOINT + 'ping/post', creds, {
+            headers: headers
+        })
+            .map(res => res.json())
+            .subscribe(
+                data => {
+                    this.receiveName = JSON.parse(data);
+                },
+                err => this.logError(err.json().message),
+                () => console.log('sent name')
+            );
+    }
+
+
+    logout() {
+        localStorage.removeItem('token');
+        //reload page NOT GOOD to change with the time
+        location.reload();
+    }
 }
--- app/login.component.ts
+++ app/login.component.ts
@@ -1,101 +1,106 @@
-import {Component, Injector, DynamicComponentLoader} from 'angular2/core';
+import {Component, Injector, DynamicComponentLoader, OnInit, OnDestroy} from 'angular2/core';
 
 import {Http, Response, Headers} from 'angular2/http';
 import {AppSettings} from './app.settings';
 import {AppComponent} from './app.component';
 
 //call md5.js
-declare var md5: any;
+declare var md5:any;
 
 @Component({
     selector: 'login',
     templateUrl: 'template/login.html'
 })
-export class LoginComponent {
-
- 
-  public receiveName;
-
-  login: string;
-  password: string;
-
-  tokenValidate: boolean;
+export class LoginComponent implements OnInit, OnDestroy {
 
 
-  public dcl;
-  public injector;
+    public receiveName;
 
-  constructor(dcl: DynamicComponentLoader, injector: Injector, private http:Http) {
-    this.dcl = dcl;
-    this.injector = injector;
-  }
+    login:string;
+    password:string;
 
-  ngOnInit() {
-    this.postToken()
-  }
+    tokenValidate:boolean;
 
-  logError(err) {
-    console.error('There was an error: ' + err);
-  }
 
-  saveToken(token) {
-    if(token) {
-      localStorage.setItem('token', token)
-      this.tokenValidate = true;
+    public dcl;
+    public injector;
+
+
+    constructor(dcl:DynamicComponentLoader, injector:Injector, private http:Http) {
+        this.dcl = dcl;
+        this.injector = injector;
     }
-  }
 
-  authenticate(login, password) {
+    ngOnDestroy() {
+        console.log('ngOnDestroy');
+    }
 
-    let creds = JSON.stringify({ login: login.value, password: md5(password.value) });
+    ngOnInit() {
+        this.postToken()
+    }
 
-    let headers = new Headers();
-    //headers.append('Content-Type', 'application/json');
-    headers.append('Content-Type' , 'application/x-www-form-urlencoded; charset=UTF-8');
+    logError(err) {
+        console.error('There was an error: ' + err);
+    }
 
-    this.http.post(AppSettings.API_ENDPOINT + 'user/login', creds, {
-      headers: headers
-    }).map(res => res.json())
-      .subscribe(
-          data => {
-            this.saveToken(JSON.parse(data).token);
-            login.value = null;
-            password.value = null;
+    saveToken(token) {
+        if (token) {
+            localStorage.setItem('token', token)
+            this.tokenValidate = true;
+        }
+    }
 
-            if(this.tokenValidate === true){
-              this.dcl.loadAsRoot(AppComponent, 'my-app', this.injector);
-            }
-          },
-          err => this.logError(err.json().message),
-          () => console.log('Authentication Complete')
-      );
-  }
+    authenticate(login, password) {
+
+        let creds = JSON.stringify({login: login.value, password: md5(password.value)});
+
+        let headers = new Headers();
+        //headers.append('Content-Type', 'application/json');
+        headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+        this.http.post(AppSettings.API_ENDPOINT + 'user/login', creds, {
+            headers: headers
+        }).map(res => res.json())
+            .subscribe(
+                data => {
+                    this.saveToken(JSON.parse(data).token);
+                    login.value = null;
+                    password.value = null;
+
+                    if (this.tokenValidate === true) {
+                        this.dcl.loadAsRoot(AppComponent, 'my-app', this.injector);
+                    }
+                },
+                err => this.logError(err.json().message),
+                () => console.log('Authentication Complete')
+            );
+    }
 
 
-  postToken() {
+    postToken() {
 
-    let token = localStorage.getItem('token');
+        let token = localStorage.getItem('token');
 
-    let creds = JSON.stringify({ token : token});
+        let creds = JSON.stringify({token: token});
 
-    let headers = new Headers();
-    //headers.append('Content-Type', 'application/json');
-    headers.append('Content-Type' , 'application/x-www-form-urlencoded; charset=UTF-8');
+        let headers = new Headers();
+        //headers.append('Content-Type', 'application/json');
+        headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
-    this.http.post(AppSettings.API_ENDPOINT + 'user/isValideToken', creds, {
-      headers: headers
-    }).map(res => res.json())
-        .subscribe(
-            data => {
-              this.tokenValidate = (JSON.parse(data).valide);
+        this.http.post(AppSettings.API_ENDPOINT + 'user/isValideToken', creds, {
+            headers: headers
+        }).map(res => res.json())
+            .subscribe(
+                data => {
+                    this.tokenValidate = (JSON.parse(data).valide);
 
-              if(this.tokenValidate === true){
-                  this.dcl.loadAsRoot(AppComponent, 'my-app', this.injector);
-              }
-            },
-            err => this.logError(err.json().message),
-            () => console.log('Checking token done')
-        );
-  }
+                    if (this.tokenValidate === true) {
+                        this.dcl.loadAsRoot(AppComponent, 'my-app', this.injector);
+                    }
+                },
+                err => this.logError(err.json().message),
+                () => console.log('Checking token done')
+            );
+    }
 
 }
