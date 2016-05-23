4d4f8c491f7b5420e3c2d9ea56a19b3c77d5977c
Alexandre <alexandre.chevallier4@gmail.com>
2016-05-23 10:05:12 +0200

    liste

--- app/liste_idees.component.ts
+++ app/liste_idees.component.ts
@@ -2,17 +2,43 @@ import {Component} from 'angular2/core';
 import {Http, Response, Headers} from 'angular2/http';
 import {AppSettings} from './app.settings';
 import {Router} from 'angular2/router';
+import {Json} from "angular2/src/facade/lang";
 
 @Component({
     templateUrl: 'template/liste.html'
 })
 export class ListeIdeesComponent{
 
+    public ideasJson = [];
 
     constructor(private router: Router,private http:Http) {
     }
+    public dataParsed = [];
 
     ngOnInit() {
+        this.getIdeasPosted();
+    }
 
+
+    getIdeasPosted() {
+
+        let creds = '';
+
+        let headers = new Headers();
+
+        headers.append('token', localStorage.getItem('token'));
+        headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+        this.http.post(AppSettings.API_ENDPOINT + 'idea', creds, {
+            headers: headers
+        }).map(res => res.json())
+            .subscribe(
+                data => {
+                    console.log(data)
+                    this.dataParsed = JSON.parse(data);
+                },
+                err => err.json().message,
+                () => console.log('Authentication Complete')
+            );
     }
 }
